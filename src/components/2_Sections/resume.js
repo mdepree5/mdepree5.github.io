9089dde6638df4a42aba7f7923ac7196b47098e6
Mitch <mdepree315@gmail.com>
2022-04-24 18:54:58 -0400

    add resume cards

--- src/components/2_Sections/resume.js
+++ src/components/2_Sections/resume.js
@@ -1,4 +1,4 @@
-import {skills, experiences, education} from '../../data/projects' 
+import {skills, experiences, education} from '../../data/resume' 
 import './Sections.css'
 // ???? ——————————————————————————————————————————————————————————————————————————————————
 
@@ -10,17 +10,40 @@ const Resume = () => {
     
     <div className='col-list'>
       <div>Skills</div>
+        {skills.map(skill => <SkillCard skill={skill}/>)}
+
       <div>Experiences</div>
+
       <div>Education</div>
+
     </div>
   </>)
 }
 
 
+
+const SkillCard = () => {
+  
+  return (
+    <div className='skill-card'>
+      Skill
+    </div>
+  )
+}
+
+const ExperienceCard = () => {
+  
+  return (
+    <div className='experience-card'>
+      Experience
+    </div>
+  )
+}
+
 const EducationCard = () => {
   
   return (
-    <div>
+    <div className='education-card'>
       Education
     </div>
   )
