e4e0b7e752fe95319c9224f2faf350b7d4dd4642
Mitch <mdepree315@gmail.com>
2022-04-24 20:39:09 -0400

    populate data

--- src/data/resume.js
+++ src/data/resume.js
@@ -9,22 +9,24 @@ export const experiences = [
   {
     name: 'McDonalds',
     role: 'General Manager',
-    description: 'Here at TVHey, we believe in the power of communication, and we believe in giving you the tools to communicate. Check out our latest product, a beautifully designed, lightning-fast messaging app (inspired by Slack). We think you\'ll love it.',
+    timeframe: 'September 2020 - February 2021',
     bullets: [
-      'thing 1',
-      'thing 2',
-      'thing 3',
+      'Strategically led shifts to meet company labor cost goals and facilitate ongoing employee training',
+      'Organized employee schedules to ensure balanced shifts during high turnover rates',
+      'Handled business communication with vendors, suppliers, bank, and law enforcement',
+      'Restructured weekly inventory and order processes from 7 hours to less than 3 hours',
     ],
     image: '',
   },
   {
     name: 'Gull Lake Ministries',
-    role: 'General Manager',
-    description: 'Here at TVHey, we believe in the power of communication, and we believe in giving you the tools to communicate. Check out our latest product, a beautifully designed, lightning-fast messaging app (inspired by Slack). We think you\'ll love it.',
+    role: 'Guest Services, Housekeeper, Food Service Director, Grounds Technician, Ropes Course Facilitator',
+    timeframe: 'May 2018 - September 2021',
     bullets: [
-      'thing 1',
-      'thing 2',
-      'thing 3',
+      'Simplified and digitized inventory procedures using Trello and Microsoft Office automation',
+      'Managed 100+ person staff in food service operations, serving 300-500 people on a daily basis',
+      'Co-developed and implemented organization-wide COVID procedures for food service',
+      'Facilitated training and course management for high ropes course and indoor rock wall',
     ],
     image: '',
   },
@@ -34,8 +36,16 @@ export const experiences = [
 export const education = [
   {
     name: 'Hope College',
-    timeframe: 'Mitch DePree',
-    description: 'Here at TVHey, we believe in the power of communication, and we believe in giving you the tools to communicate. Check out our latest product, a beautifully designed, lightning-fast messaging app (inspired by Slack). We think you\'ll love it.',
+    timeframe: 'August 2015 - May 2019',
+    description: 'BA: Classical Studies, Religion - Minors: Chemistry, Communication',
+    link: 'https://hope.edu/',
+    image: '',
+  },
+  {
+    name: 'App Academy',
+    timeframe: 'September 2021 - May 2022',
+    description: 'Immersive software development course with emphasis on full stack web development',
+    link: 'https://www.appacademy.io/',
     image: '',
   },
 ]
