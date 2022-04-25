ee5bbbb05564082c04d23e0768e5cbb0c62e7aca
Mitch DePree <mdepree315@gmail.com>
2022-04-25 15:35:38 -0400

    style footer

--- src/components/1_Navigation/index.js
+++ src/components/1_Navigation/index.js
@@ -15,9 +15,6 @@ const Navigation = ({mode, switchMode}) => {
   return (
     <nav className={`${mode}-nav nav-bar`}>
       <div id='left-nav'>
-    {/* <img className='navicon' onClick={() => history.push('/')}
-      src='https://capstone-slack-clone.s3.amazonaws.com/favicon.ico' alt='custom'
-    /> */}
         <h2 style={{cursor:'pointer'}} onClick={() => navigate('/')} >Mitch DePree</h2>
       </div>
     
--- src/components/3_Footer/index.js
+++ src/components/3_Footer/index.js
@@ -1,18 +1,21 @@
+import { MdOutgoingMail } from "react-icons/md";
 import './Footer.css'
 // ???? ——————————————————————————————————————————————————————————————————————————————————
 
 const Footer = ({mode}) => {
   return (
     <footer className={`${mode}-footer footer`}>
-      <h2>Contact</h2>
-      <ExternalLink link='https://github.com/mdepree5' image='https://capstone-slack-clone.s3.amazonaws.com/github.png' />
-      <ExternalLink link='https://www.linkedin.com/in/mitch-depree-4a5686155/' image='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg' />
-      <a href="mailto:mdepree315@gmail.com" >Email Mitch</a>
+      <h2 style={{marginLeft: '1em'}}>Contact</h2>      
+      
+      <div className='footer-links'>
+        <img style={{cursor:'pointer', height:'1.9em', width:'1.9em', backgroundColor:'white', borderRadius:'0.15em' }} onClick={()=>window.open('https://github.com/mdepree5')} src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg' alt='about-link' />
+        <img style={{cursor:'pointer', height:'2em', width:'2em'}} onClick={()=>window.open('https://www.linkedin.com/in/mitch-depree-4a5686155/')} src={'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg'} alt='about-link' />
+        <a href="mailto:mdepree315@gmail.com" ><MdOutgoingMail style={{height: '2.6em', width:'2.6em', cursor: 'pointer'}}/></a>
+      </div>
 
     </footer>
   )
 }
 
-const ExternalLink = ({link, image}) => <img style={{cursor:'pointer', height:'2em', width:'2em'}} onClick={()=>window.open(link)} src={image} alt='about-link' />
 
 export default Footer;
