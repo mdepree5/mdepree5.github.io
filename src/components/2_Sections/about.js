a1f784552b2e2194f321078125c8f372d66fcd1a
Mitch DePree <mdepree315@gmail.com>
2022-04-25 20:48:34 -0400

    style about

--- src/components/2_Sections/about.js
+++ src/components/2_Sections/about.js
@@ -6,9 +6,10 @@ import './Sections.css'
 const About = () => {
   return (<>
     <h1>Mitch DePree is a software developer and lifelong learner
-      who does x in order for y to happen
+      who needs to write more about his about
+      {/* who does x in order for y to happen */}
     </h1>
-{/*     
+    {/*     
     <h2>Software Developer</h2>
 
     <h2>Lifelong Learner</h2>
@@ -19,6 +20,8 @@ const About = () => {
     <h2>Things Mitch is currently working on:</h2>
     <div>Trying to learn recursion.</div>
     <div>Trying to learn recursion.</div> */}
+
+    <img style={{width: '40em', height:'30em'}} src="https://static.seekingalpha.com/uploads/2013/3/21/7360901-13638972437431467-Robert-Wagner.jpg" alt="" />
 
   </>)
 }
