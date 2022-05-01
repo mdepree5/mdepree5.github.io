a5463257b7334f8420d4f258f86df4d6b988be0c
Mitch DePree <mdepree315@gmail.com>
2022-05-01 17:07:25 -0400

    abstract home/about pages

--- src/components/2_Sections/about.js
+++ src/components/2_Sections/about.js
@@ -6,11 +6,8 @@ const About = () => {
     <h1>Mitch DePree is a software developer and lifelong learner
       who is committed to building a better world
     </h1>
-    
-    <div className='about-page'>
+    <div className='info-page'>
       <AboutText />
-      <img className='home-image' src="https://static.seekingalpha.com/uploads/2013/3/21/7360901-13638972437431467-Robert-Wagner.jpg" alt="" />
-      
       <img style={{height:'10em', width:'9em', borderRadius:'0.5em'}} src='https://md5-self.s3.amazonaws.com/self/headshot.jpg' alt='' />
     </div>
   </>)
@@ -19,17 +16,15 @@ const About = () => {
 
 const AboutText = () => {
   return (
-    <div className='about-text'>
+    <div className='info-text'>
       <div>
         Growing up,
         Mitch did x, y, z
         Mitch found 1, 2, 3 interesting
-
       </div>
 
       <div>
         Mitch earned Bachelor of Arts degrees in Classical Studies and Religion from Hope College.
-        
         After studying at Hope College
       </div>
 
--- src/components/2_Sections/home.js
+++ src/components/2_Sections/home.js
@@ -5,16 +5,24 @@ import './Sections.css'
 const Home = () => {
   return (<>
     <h1>Engineer, Developer</h1>
-    <div className='home-page'>
-      <div className='manifesto'>
-        Mitch deletes as much code as he writes,
-        trades indecisiveness for automation, and 
-        reads as much documentation as a 12oz medium roast will take him on a Saturday morning.
-      </div>
-      <img className='home-image' src='https://md5-self.s3.amazonaws.com/self/header.jpeg' alt='' />
+    <div className='info-page'>
+      <HomeText/>
+      <img className='info-image' src='https://md5-self.s3.amazonaws.com/self/header.jpeg' alt='' />
     </div>
     <h2>Writing better code to build a brighter world</h2>
   </>)
 }
 
+
+const HomeText = () => {
+  return (
+    <div className='info-text'>
+      <div>
+        Mitch deletes as much code as he writes,
+        trades indecisiveness for automation, and 
+        reads as much documentation as a 12oz medium roast will take him on a Saturday morning.
+      </div>
+    </div>
+  )
+}
 export default Home;
