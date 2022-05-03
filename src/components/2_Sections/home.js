3725aa571da74754b3dca1d70411a3846bd41fed
Mitch DePree <mdepree315@gmail.com>
2022-05-03 00:56:57 -0400

    restyle h1

--- src/components/2_Sections/home.js
+++ src/components/2_Sections/home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className='info-page' id='home'>
       <h1>
-        Always learning, tinkering, and improving.
+        Always learning. Always tinkering. Always improving.
       </h1>
       <img className='info-image' src='https://md5-self.s3.amazonaws.com/self/header.jpeg' alt='' />
     </div>
